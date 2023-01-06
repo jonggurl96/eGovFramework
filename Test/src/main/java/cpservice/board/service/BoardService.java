110ded216bfccaff3e1938faa7476d1b7be6c380
jonggurl96 <jonggurl96@naver.com>
2023-01-06 09:57:54 +0900

    first commit

---
+++ Test/src/main/java/cpservice/board/service/BoardService.java
@@ -0,0 +1,25 @@
+package cpservice.board.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cpservice.board.domain.BoardVO;
+
+@Service
+public interface BoardService {
+
+	public BoardVO select(int bno) throws Exception;
+	
+	public void regist(BoardVO vo) throws Exception;
+	
+	public void modify(BoardVO vo) throws Exception;
+	
+	public void remove(int bno) throws Exception;
+	
+	public List<BoardVO> listAll() throws Exception;
+	
+	public int getCount() throws Exception;
+	
+	public List<BoardVO> getList(int start, int rcpp) throws Exception;
+}
