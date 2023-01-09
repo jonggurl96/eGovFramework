ba26de5b519ed206e8f4fe6a231214ef1b9d924b
jonggurl96 <jonggurl96@naver.com>
2023-01-09 12:54:04 +0900

    delete by ajax

--- Test/src/main/java/cpservice/board/web/ReplyController.java
+++ Test/src/main/java/cpservice/board/web/ReplyController.java
@@ -53,5 +53,34 @@ public class ReplyController {
 		}
 		return entity;
 	}
+	
+	@RequestMapping(value="/{rno}", method= {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<String> update(@PathVariable("rno") int rno, @RequestBody ReplyVO vo) {
+		ResponseEntity<String> entity = null;
+		try {
+			vo.setRno(rno);
+			service.modifyReply(vo);
+			
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	
+	@RequestMapping(value="/{rno}", method=RequestMethod.DELETE)
+	public ResponseEntity<String> remove(@PathVariable("rno") int rno) {
+		ResponseEntity<String> entity = null;
+		
+		try {
+			service.removeReply(rno);
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
 
 }
