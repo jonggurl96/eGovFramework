4dd38e07d85e6997b641c4a9bea6683f63bcc435
jonggurl96 <jonggurl96@naver.com>
2023-01-09 12:31:49 +0900

    ajax list all

--- Test/src/main/java/cpservice/board/web/ReplyController.java
+++ Test/src/main/java/cpservice/board/web/ReplyController.java
@@ -1,11 +1,14 @@
 package cpservice.board.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,6 +38,18 @@ public class ReplyController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	
+	@RequestMapping(value="/all/{bno}", method=RequestMethod.GET)
+	public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") int bno) {
+		ResponseEntity<List<ReplyVO>> entity = null;
+		try {
+			entity = new ResponseEntity<List<ReplyVO>>(service.listReply(bno), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		return entity;
 	}
