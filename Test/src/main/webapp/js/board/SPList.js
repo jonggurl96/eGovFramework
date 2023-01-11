a62c01edeae4afdc21ebec3d69306c7548354eaa
jonggurl96 <jonggurl96@naver.com>
2023-01-11 17:44:06 +0900

    readme, user profile

---
+++ Test/src/main/webapp/js/board/SPList.js
@@ -0,0 +1,111 @@
+/**
+ * ajax를 이용한 페이징 게시판
+ */
+
+let getDate = (num) => {
+	let date = new Date(num);
+	
+	let month = date.getMonth() + 1;
+	let day = date.getDate();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	
+	month = month < 10 ? "0" + month : month;
+	day = day < 10 ? "0" + day : day;
+	hours = hours < 10 ? "0" + hours : hours;
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+
+	return date.getFullYear() + "-" + month + "-" + day + " " + hours + ":" + minutes;
+}
+
+let downloadPage = function (tag, keyword, page, rcpp, totalRecordCount) {
+	
+	let data = {
+			page,
+			rcpp,
+			totalRecordCount
+	};
+	if(tag === "all") {
+		$('#keyword').val("");
+	}
+	else {
+		data["tag"] = tag;
+		data["keyword"] = keyword;
+	}
+	
+	$.ajax({
+		type: "post",
+		url: "/search",
+		contentType: "application/json",
+		dataType: "text",
+		data: JSON.stringify(data),
+		success: function(data) {
+		
+			$('#table-board.removable').remove();
+			
+			data = JSON.parse(data);
+			let str = "";
+			
+			data.forEach(element => {
+				let href = "/board/read?bno=" + element["bno"] + "&page=" + page + "&rcpp=" + rcpp;
+				str += "<tr class='removable'><td class='td-bno'>" + element["bno"] + "</td>";
+				str += "<td><a href=" + href + ">" + element["title"] + "</a></td>";
+				str += "<td>" + element["writer"] + "</td>";
+				str += "<td>" + getDate(element["regdate"]) + "</td>";
+			});
+			$('#table-board').append(str);
+		}
+	});
+}
+
+$(document).ready(function() {
+	$('select option').each(function() {
+		if($(this).val() == "${pageInfo.recordCountPerPage}") {
+			$(this).attr("selected", "selected");
+		}
+	});
+	let tag = $('#tag').val();
+	let keyword = $('#keyword').val();
+	let rcpp = $('#rcpp').val();
+	let pageNo = $('#paging strong').text();
+	let totalRecordCount = $('#totalRecordCount').val();
+	downloadPage(tag, keyword, pageNo, rcpp, totalRecordCount);
+	
+});
+
+let redirect = (tag, keyword, pageNo, rcpp) => {
+	let redirectURL = "/board/SPList?page=" + pageNo + "&rcpp=" + rcpp;
+	if(!(tag === "all")) {
+		redirectURL += "&tag=" + tag + "&keyword=" + keyword;
+	}
+	self.location = redirectURL;
+}
+
+function otherPage(pageNo) {
+	let tag = $('#tag').val();
+	let keyword = $('#keyword').val();
+	let rcpp = $('#rcpp').val();
+	console.log(pageNo);
+	
+	redirect(tag, keyword, pageNo, rcpp);
+}
+
+let searchKeyword = () => {
+	otherPage(1);
+}
+
+let changeRCPP = (sel) => {
+
+	let currentPageNo = $('#paging strong').text();
+	let preRcpp = $('#currentPageNo').val();
+	
+	let boardIndex = ( currentPageNo - 1) * preRcpp + 1;
+	let newPage = Math.ceil(boardIndex / sel.value);
+	
+	let tag = $('#tag').val();
+	let keyword = $('#keyword').val();
+	
+	preRcpp.val(sel.value);
+	
+	redirect(tag, keyword, newPage, sel.value);
+}
---
+++ Test/src/main/webapp/js/board/read.js
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	let form = $("form[role='form']");
+	let writer = $('#writer').val();
+	let id = $('#login-id').text();
+	
+	$(".btn-warning").on("click", function() {
+		if(writer != id) {
+			alert("수정 불가");
+			return;
+		}
+		form.attr("action", "/board/modify");
+		form.attr("method", "get");
+		form.submit();
+	});
+	$(".btn-danger").on("click", function() {
+		if(writer != id) {
+			alert("삭제 불가");
+			return;
+		}
+		form.attr("action", "/board/remove");
+		form.submit();
+	});
+	$(".btn-primary").on("click", function() {
+		let page = $('#page').val();
+		let rcpp = $('#rcpp').val();
+		self.location = "/board/SPList?page=" + page + "&rcpp=" + rcpp;
+	});
+	
+	getReplies();
+});
+let getReplies = () => {
+	let bno = $('#bno').val();
+	let delBtnVal = $('#delButton').val();
+	
+	$.ajax({
+		type: "get",
+		url: '/replies/all/' + bno,
+		success: function(data) {
+			let str = "";
+			$(data).each(function() {
+				str += "<li data-rno=" + this.rno + " class='ReplyList'>" + this.rno + ":" + this.replytext + "---" + this.replyer;
+				str += "<button onclick='deleteReply(" + this.rno + ", &quot;" + this.replyer + "&quot;);'>" + delBtnVal + "</button></li>";
+			});
+			$('#replies').html(str);
+		}
+	});
+}
+let deleteReply = (rno, replyer) => {
+	let id = $('#login-id').text();
+	if(id != replyer) {
+		alert("작성자만 삭제 가능합니다.");
+		return;
+	}
+	$.ajax({
+		type: "delete",
+		url: "/replies/" + rno,
+		success: function(data) {
+			alert(data);
+			getReplies();
+		}
+	});
+}
+let writeReply = () => {
+	let replyer = $('#login-id').text();
+	let replytext = $('#writtenReply').val();
+	let bno = $('#bno').val();
+	$.ajax({
+		type:"post",
+		url:"/replies/",
+		headers: {
+			"Content-Type":"application/json",
+			"X-HTTP-Method-Override": "post"
+		},
+		dataType: "text",
+		data: JSON.stringify({
+			replytext,
+			replyer,
+			bno
+		}),
+		success: function(msg) {
+			alert(msg);
+			if(msg == "SUCCESS") {
+				getReplies();
+			}
+			$('#writtenReply').val("");
+		}
+	});
+}
