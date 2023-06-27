b573a450289814faedf7f4e29af4f48bc2718c09
jonggurl96 <jonggurl96@naver.com>
2023-06-27 11:06:20 +0900

    initial commit

---
+++ src/main/java/com/zzong/egovframework/EGovFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.zzong.egovframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGovFrameworkApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EGovFrameworkApplication.class, args);
+    }
+    
+}
