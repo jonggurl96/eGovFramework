d90811c4a94e9e3148c8a3701b38f7621b5ae8dc
jonggurl96 <jonggurl96@naver.com>
2023-06-30 08:55:04 +0900

    custom logger 추가

--- src/main/java/com/zzong/egovframework/web/persistence/jpa/PostController.java
+++ src/main/java/com/zzong/egovframework/web/persistence/jpa/PostController.java
@@ -2,6 +2,8 @@ package com.zzong.egovframework.web.persistence.jpa;
 
 import com.zzong.egovframework.persistence.dto.PostDTO;
 import com.zzong.egovframework.persistence.service.PostService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,11 @@ public class PostController {
     
     @GetMapping("/start/{kwrd}")
     public List<PostDTO> findStartsWith(@PathVariable String kwrd) {
-        return postService.getAll(kwrd);
+        Logger logger = LogManager.getLogger();
+        logger.info("Using JPA, find all post starting with {}", kwrd);
+    
+        List<PostDTO> retVal = postService.getAll(kwrd);
+        logger.debug("Returned value from http://localhost:8080/jpa/post/start/fb7: {}", retVal);
+        return retVal;
     }
 }
