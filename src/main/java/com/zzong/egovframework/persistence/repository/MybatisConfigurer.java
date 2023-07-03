99326d9f670e7c8c033f6b9c776afc72c3329155
jonggurl96 <jonggurl96@naver.com>
2023-07-03 12:59:05 +0900

    @Configuration class @Bean 메서드 전달인자에 @Value 추가

--- src/main/java/com/zzong/egovframework/persistence/repository/MybatisConfigurer.java
+++ src/main/java/com/zzong/egovframework/persistence/repository/MybatisConfigurer.java
@@ -1,24 +1,25 @@
 package com.zzong.egovframework.persistence.repository;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.egovframe.rte.psl.dataaccess.mapper.MapperConfigurer;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.Resource;
-
+import javax.sql.DataSource;
 
 @Configuration
 public class MybatisConfigurer {
     
     @Bean("sqlSession")
     @Resource(name = "dataSource")
-    public SqlSessionFactoryBean sqlSessionFactoryBean(HikariDataSource dataSource) {
+    public SqlSessionFactoryBean sqlSessionFactoryBean(@Value("${mybatis.config.classpath}") String mcc,
+                                                       DataSource dataSource) {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         
-        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("persistence/mybatis/mybatis_config.xml"));
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(mcc));
         
         sqlSessionFactoryBean.setDataSource(dataSource);
         return sqlSessionFactoryBean;
